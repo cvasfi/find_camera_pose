f26ba9cab38f6b9626ef57e56e3129607156e4f2
cvasfi <w159753>
2016-12-26 17:46:53 +0100

    Importing project to github
    Dont forget to create  these folders: frames, images, results in the
    proejct directory.

---
+++ src/find_camera_pose.cpp
@@ -0,0 +1,216 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "sensor_msgs/Image.h"
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/opencv.hpp>
+#include "lsd_slam_viewer/keyframeMsg.h"
+#include <sstream>
+#include <fstream>
+#include <math.h>
+#include <Eigen/Geometry>
+#include "sophus/sim3.hpp"
+#include "sophus/se3.hpp"
+#include <boost/filesystem.hpp>
+    #include <rosbag/bag.h>
+#include <boost/foreach.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+    #include <rosbag/view.h>
+#include <ctime>
+
+#include "matching.h"
+
+struct InputPointDense {
+        float idepth;
+        float idepth_var;
+        unsigned char color[4];
+};
+
+Vector<cv::Point2f> queryPoints;
+Vector<cv::Point2f> match2dPoints;
+
+rosbag::Bag frameBag;
+cv_bridge::CvImagePtr cv_ptr;
+//int i=0;
+  std::ofstream imageList;
+
+void imageCB(const sensor_msgs::Image::ConstPtr& msg)
+{
+    std::ostringstream oss;
+  std::string filename="im";
+  try
+  {
+    cv_ptr    = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::MONO8);
+  }
+  catch (cv_bridge::Exception& e)
+  {
+    ROS_ERROR("cv_bridge exception: %s", e.what());
+    return;
+  }
+    oss << "src/find_camera_pose/images/im" << msg->header.frame_id.data()<<".png";
+    cv::imwrite( oss.str(), cv_ptr->image );
+    imageList << "im"<<msg->header.frame_id.data()<<".png"<<std::endl;
+
+    //savepcl
+//    i++;
+
+}
+
+void frameCB(const lsd_slam_viewer::keyframeMsgConstPtr& msg)
+{
+
+    frameBag.write("frames", ros::Time::now(), *msg);
+
+
+}
+
+
+void calculate3DPoints(const std::vector<cv::Point2f>& qCoordinates, lsd_slam_viewer::keyframeMsgConstPtr& frame, int width, int height){
+    InputPointDense *points = (InputPointDense*)frame->pointcloud.data();
+    Sophus::Sim3f camToWorld;
+    memcpy(camToWorld.data(), frame->camToWorld.data(), 7*sizeof(float));
+
+    float my_scale = camToWorld.scale(); // TODO lsd: maybe add maximum-likelyhood scaling factor...
+    // float my_scaledTH = exp10(-2.5);
+    float my_scaledTH = exp10(-3.0);
+    float my_scaledTH2 = exp10(-2.0);
+    int my_minNearSupport = 7;
+    float my_absTH = exp10(-1.0);
+    float my_absTH2 = exp10(-1.0);
+    int sparsify1=2;
+    int sparsify2=5;
+    double maxDist = 0;
+    int otherMinNearSupport = 9;
+
+    int fxi=1/frame->fx;
+    int fyi=1/frame->fy;
+    int cxi=-frame->cx/frame->fx;
+    int cyi=-frame->cy/frame->fy;
+
+    for(size_t i;i<qCoordinates.size();i++){
+        int x=qCoordinates[i].x;
+        int y=qCoordinates[i].y;
+
+        if (points[x + y * width].idepth <= 0)
+                continue;
+
+        float depth = 1 / points[x + y * width].idepth;
+        float depth4 = depth * depth;
+        depth4 *= depth4;
+        bool unaccurate = false;
+
+        if (points[x + y * width].idepth_var * depth4 > my_scaledTH)
+                unaccurate = true;
+
+        if (points[x + y * width].idepth_var * depth4 * my_scale
+                        * my_scale > my_absTH)
+                unaccurate = true;
+
+        if (points[x + y * width].idepth_var * depth4 > my_scaledTH2)
+                continue;
+
+        if (points[x + y * width].idepth_var * depth4 * my_scale
+                        * my_scale > my_absTH2)
+                continue;
+
+        if (unaccurate) {
+                if (sparsify2 > 1 && rand() % sparsify2 != 0)
+                        continue;
+        } else {
+                if (sparsify1 > 1 && rand() % sparsify1 != 0)
+                        continue;
+        }
+
+        if (my_minNearSupport > 1) {
+                int nearSupport = 0;
+                for (int dx = -1; dx < 2; dx++)
+                        for (int dy = -1; dy < 2; dy++) {
+                                int idx = x + dx + (y + dy) * width;
+                                if (points[idx].idepth > 0) {
+                                        float diff = points[idx].idepth - 1.0f / depth;
+                                        if (diff * diff
+                                                        < 2 * points[x + y * width].idepth_var)
+                                                nearSupport++;
+                                }
+                        }
+
+                if (nearSupport < my_minNearSupport
+                                || (unaccurate && nearSupport < otherMinNearSupport))
+                        continue;
+        }
+
+        double idepthVar = points[x + y * width].idepth_var;
+        if (idepthVar > my_scaledTH) {
+                // std::cout << "Var... " << idepthVar << " . depth: " << depth << std::endl;
+        }
+
+        if (unaccurate) {
+                depth = 1 / (points[x + y * width].idepth + 0.1);
+                depth *= 0.8;
+        }
+
+        float posX = (x * fxi + cxi) * depth;
+        float posY = (y * fyi + cyi) * depth;
+        float posZ = depth;
+
+        if (depth > maxDist) {
+                maxDist = depth;
+        }
+
+        Eigen::Vector3f vec(posX, posY, posZ);
+        Eigen::Vector3f realPos = camToWorld * vec;
+    }
+
+}
+
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "find_camera_pose");
+
+  ros::NodeHandle n;
+  ros::Time recordBegin = ros::Time::now();
+  ros::Time recordEnd = ros::Time::now();
+  ros::Duration recordTime(20);    //record for 3 minutes
+  ros::Rate r(10); // 10 hz
+
+  imageList.open("src/find_camera_pose/images/trainImageList.txt", std::ios_base::app);
+  frameBag.open("src/find_camera_pose/frames/frames.bag", rosbag::bagmode::Write);
+   //   frameBag.open("src/find_camera_pose/frames/frames.bag", rosbag::bagmode::Read);
+
+ // rosbag::View view(frameBag, rosbag::TopicQuery("frames"));
+
+//  BOOST_FOREACH(rosbag::MessageInstance const m, view)
+//  {
+//      lsd_slam_viewer::keyframeMsg::ConstPtr frame = m.instantiate<lsd_slam_viewer::keyframeMsg>();
+//      if (frame != NULL)
+//          std::cout<<"read frame with id: "<<frame->id<<std::endl;
+//      if(frame->id==213){
+//          std::cout<<"I FOUND THE BEST MATCH!!!!"<<std::endl;
+//      }
+
+//  }
+//  frameBag.close();
+
+  ros::Subscriber subImages = n.subscribe("/lsd_slam/keyImages", 1000, imageCB);
+  ros::Subscriber subFrames = n.subscribe("/lsd_slam/keyframes", 1000, frameCB);
+
+  //mtmi()
+  while(ros::ok() && (recordEnd-recordBegin)<recordTime){
+      ros::spinOnce();
+      r.sleep();
+      recordEnd = ros::Time::now();
+  }
+  subImages.shutdown();
+  subFrames.shutdown();
+  //record is over
+
+    findMatch();
+    queryPoints=getQueryCoordinates();
+    match2dPoints=getMatchCoordinates();
+ // ros::spin();
+
+    std::cout<<"dif is: "<<(recordEnd-recordBegin);
+  return 0;
+}
---
+++ src/matching.h
@@ -0,0 +1,289 @@
+#ifndef MATCHING_H    // To make sure you don't declare the function more than once by including the header multiple times.
+#define MATCHING_H
+
+
+
+
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/contrib/contrib.hpp"
+#include "opencv2/nonfree/nonfree.hpp"
+
+#include <iostream>
+#include <fstream>
+
+using namespace cv;
+using namespace std;
+
+const string defaultDetectorType = "SIFT";
+const string defaultDescriptorType = "SIFT";
+const string defaultMatcherType = "FlannBased";
+const string defaultQueryImageName = "src/find_camera_pose/query.png";
+const string defaultFileWithTrainImages = "src/find_camera_pose/images/trainImageList.txt";
+const string defaultDirToSaveResImages = "src/find_camera_pose/results";
+
+Vector<cv::Point2f> qCoordinates;
+Vector<cv::Point2f> tCoordinates;
+
+struct trainImgIdx_equal {
+    int comp_to;
+
+    trainImgIdx_equal(int a) : comp_to(a) {}
+
+    bool operator()(DMatch p) {
+        return p.imgIdx == comp_to;
+    }
+};
+
+static void maskMatchesByTrainImgIdx( const vector<DMatch>& matches, int trainImgIdx, vector<char>& mask )
+{
+    int ii=1;
+    mask.resize( matches.size() );
+    fill( mask.begin(), mask.end(), 0 );
+    for( size_t i = 0; i < matches.size(); i++ )
+    {
+        if( matches[i].imgIdx == trainImgIdx ){
+            mask[i] = 1;
+            ii++;
+        }
+    }
+  //  cout<<" is: "<<ii<<endl;
+}
+
+
+static void readTrainFilenames( const string& filename, string& dirName, vector<string>& trainFilenames )
+{
+    trainFilenames.clear();
+
+    ifstream file( filename.c_str() );
+    if ( !file.is_open() )
+        return;
+
+    size_t pos = filename.rfind('\\');
+    char dlmtr = '\\';
+    if (pos == String::npos)
+    {
+        pos = filename.rfind('/');
+        dlmtr = '/';
+    }
+    dirName = pos == string::npos ? "" : filename.substr(0, pos) + dlmtr;
+
+    while( !file.eof() )
+    {
+        string str; getline( file, str );
+        if( str.empty() ) break;
+        trainFilenames.push_back(str);
+    }
+    file.close();
+}
+
+static bool createDetectorDescriptorMatcher( const string& detectorType, const string& descriptorType, const string& matcherType,
+                                      Ptr<FeatureDetector>& featureDetector,
+                                      Ptr<DescriptorExtractor>& descriptorExtractor,
+                                      Ptr<DescriptorMatcher>& descriptorMatcher )
+{
+    cout << "< Creating feature detector, descriptor extractor and descriptor matcher ..." << endl;
+    featureDetector = FeatureDetector::create( detectorType );
+    descriptorExtractor = DescriptorExtractor::create( descriptorType );
+    descriptorMatcher = DescriptorMatcher::create( matcherType );
+    cout << ">" << endl;
+    cout << featureDetector << " and " << descriptorExtractor << "and " << descriptorMatcher;
+    bool isCreated = !( featureDetector.empty() || descriptorExtractor.empty() || descriptorMatcher.empty() );
+    if( !isCreated )
+        cout << "Can not create feature detector or descriptor extractor or descriptor matcher of given types." << endl << ">" << endl;
+
+    return isCreated;
+}
+
+static bool readImages( const string& queryImageName, const string& trainFilename,
+                 Mat& queryImage, vector <Mat>& trainImages, vector<string>& trainImageNames )
+{
+    cout << "< Reading the images..." << endl;
+    queryImage = imread( queryImageName, CV_LOAD_IMAGE_GRAYSCALE);
+    if( queryImage.empty() )
+    {
+        cout << "Query image can not be read." << endl << ">" << endl;
+        return false;
+    }
+    string trainDirName;
+    readTrainFilenames( trainFilename, trainDirName, trainImageNames );
+    if( trainImageNames.empty() )
+    {
+        cout << "Train image filenames can not be read." << endl << ">" << endl;
+        return false;
+    }
+    int readImageCount = 0;
+    for( size_t i = 0; i < trainImageNames.size(); i++ )
+    {
+        string filename = trainDirName + trainImageNames[i];
+        Mat img = imread( filename, CV_LOAD_IMAGE_GRAYSCALE );
+        if( img.empty() )
+            cout << "Train image " << filename << " can not be read." << endl;
+        else
+            readImageCount++;
+        trainImages.push_back( img );
+    }
+    if( !readImageCount )
+    {
+        cout << "All train images can not be read." << endl << ">" << endl;
+        return false;
+    }
+    else
+        cout << readImageCount << " train images were read." << endl;
+    cout << ">" << endl;
+
+    return true;
+}
+
+static void detectKeypoints( const Mat& queryImage, vector<KeyPoint>& queryKeypoints,
+                      const vector<Mat>& trainImages, vector<vector<KeyPoint> >& trainKeypoints,
+                      Ptr<FeatureDetector>& featureDetector )
+{
+    cout << endl << "< Extracting keypoints from images..." << endl;
+    featureDetector->detect( queryImage, queryKeypoints );
+    featureDetector->detect( trainImages, trainKeypoints );
+    cout << ">" << endl;
+}
+
+static void computeDescriptors( const Mat& queryImage, vector<KeyPoint>& queryKeypoints, Mat& queryDescriptors,
+                         const vector<Mat>& trainImages, vector<vector<KeyPoint> >& trainKeypoints, vector<Mat>& trainDescriptors,
+                         Ptr<DescriptorExtractor>& descriptorExtractor )
+{
+    cout << "< Computing descriptors for keypoints..." << endl;
+    descriptorExtractor->compute( queryImage, queryKeypoints, queryDescriptors );
+    descriptorExtractor->compute( trainImages, trainKeypoints, trainDescriptors );
+
+    int totalTrainDesc = 0;
+    for( vector<Mat>::const_iterator tdIter = trainDescriptors.begin(); tdIter != trainDescriptors.end(); tdIter++ )
+        totalTrainDesc += tdIter->rows;
+
+    cout << "Query descriptors count: " << queryDescriptors.rows << "; Total train descriptors count: " << totalTrainDesc << endl;
+    cout << ">" << endl;
+}
+
+static void matchDescriptors( const Mat& queryDescriptors, const vector<Mat>& trainDescriptors,
+                       vector<DMatch>& matches, Ptr<DescriptorMatcher>& descriptorMatcher )
+{
+    cout << "< Set train descriptors collection in the matcher and match query descriptors to them..." << endl;
+    TickMeter tm;
+
+    tm.start();
+    descriptorMatcher->add( trainDescriptors );
+    descriptorMatcher->train();
+    tm.stop();
+    double buildTime = tm.getTimeMilli();
+
+    tm.start();
+    descriptorMatcher->match( queryDescriptors, matches );
+    tm.stop();
+    double matchTime = tm.getTimeMilli();
+
+    CV_Assert( queryDescriptors.rows == (int)matches.size() || matches.empty() );
+
+    cout << "Number of matches: " << matches.size() << endl;
+    cout << "Build time: " << buildTime << " ms; Match time: " << matchTime << " ms" << endl;
+    cout << ">" << endl;
+}
+
+
+static void saveResultImages( const Mat& queryImage, const vector<KeyPoint>& queryKeypoints,
+                       const vector<Mat>& trainImages, const vector<vector<KeyPoint> >& trainKeypoints,
+                       const vector<DMatch>& matches, const vector<string>& trainImagesNames, const string& resultDir )
+{
+   // std::map<int, int> bestMatch;
+    int maxMatchNum=0;
+    int currentMatchNum;
+    int bestMatchIndex=-1;
+
+
+    cout << "< Save results..." << endl;
+    Mat drawImg;
+    vector<char> mask;
+    for( size_t i = 0; i < trainImages.size(); i++ )
+    {
+        if( !trainImages[i].empty() )
+        {
+            currentMatchNum=std::count_if(matches.begin(), matches.end(), trainImgIdx_equal((int)i));
+            if(currentMatchNum>maxMatchNum){
+                maxMatchNum=currentMatchNum;
+                bestMatchIndex=i;
+            }
+            cout<<"match size for image"<<i<<" is : "<<currentMatchNum<<endl;
+
+            maskMatchesByTrainImgIdx( matches, (int)i, mask );
+            drawMatches( queryImage, queryKeypoints, trainImages[i], trainKeypoints[i],
+                         matches, drawImg, Scalar(255, 0, 0), Scalar(0, 255, 255), mask );
+            string filename = resultDir + "/res_" + trainImagesNames[i];
+            if( !imwrite( filename, drawImg ) )
+                cout << "Image " << filename << " can not be saved (may be because directory " << resultDir << " does not exist)." << endl;
+
+        }
+    }
+    cout<<"best matching image is: "<<bestMatchIndex+1<<" with "<<maxMatchNum<<" matches"<<endl;
+    vector<DMatch> correspondingMatches;
+
+
+    for(size_t i=0;i<matches.size();i++){
+        if(matches[i].imgIdx==bestMatchIndex){
+            correspondingMatches.push_back(matches[i]);
+            qCoordinates.push_back(queryKeypoints[matches[i].queryIdx].pt);
+            tCoordinates.push_back(trainKeypoints[bestMatchIndex][matches[i].trainIdx].pt);
+        }
+    }
+
+
+
+    cout<<"size of cmatches is: "<<correspondingMatches.size()<<endl;
+    cout << ">" << endl;
+}
+
+Vector<cv::Point2f> getQueryCoordinates(){return qCoordinates;}
+Vector<cv::Point2f> getMatchCoordinates(){return tCoordinates;}
+
+
+int findMatch(){
+    initModule_nonfree();
+    string detectorType = defaultDetectorType;
+    string descriptorType = defaultDescriptorType;
+    string matcherType = defaultMatcherType;
+    string queryImageName = defaultQueryImageName;
+    string fileWithTrainImages = defaultFileWithTrainImages;
+    string dirToSaveResImages = defaultDirToSaveResImages;
+
+
+    Ptr<FeatureDetector> featureDetector;
+    Ptr<DescriptorExtractor> descriptorExtractor;
+    Ptr<DescriptorMatcher> descriptorMatcher;
+    if( !createDetectorDescriptorMatcher( detectorType, descriptorType, matcherType, featureDetector, descriptorExtractor, descriptorMatcher ) )
+    {
+        return -1;
+    }
+
+    Mat queryImage;
+    vector<Mat> trainImages;
+    vector<string> trainImagesNames;
+    if( !readImages( queryImageName, fileWithTrainImages, queryImage, trainImages, trainImagesNames ) )
+    {
+        return -1;
+    }
+
+    vector<KeyPoint> queryKeypoints;
+    vector<vector<KeyPoint> > trainKeypoints;
+    detectKeypoints( queryImage, queryKeypoints, trainImages, trainKeypoints, featureDetector );
+
+    Mat queryDescriptors;
+    vector<Mat> trainDescriptors;
+    computeDescriptors( queryImage, queryKeypoints, queryDescriptors,
+                        trainImages, trainKeypoints, trainDescriptors,
+                        descriptorExtractor );
+
+    vector<DMatch> matches;
+    matchDescriptors( queryDescriptors, trainDescriptors, matches, descriptorMatcher );
+
+    saveResultImages( queryImage, queryKeypoints, trainImages, trainKeypoints,
+                      matches, trainImagesNames, dirToSaveResImages );
+    return 0;
+
+}
+
+#endif
